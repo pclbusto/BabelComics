ad862408087dc6882d0fb5bfccf6c8a56a84f1b9
pclbusto <pcbusto@gmail.com>
2016-10-21 07:36:51 -0300

    dsad

--- KivyPublisherGui.py
+++ KivyPublisherGui.py
@@ -11,6 +11,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
+from kivy.uix.carousel import Carousel
 
 class KivyPublisherGui(Screen):
     def __init__(self,**kwargs):
@@ -19,57 +20,46 @@ class KivyPublisherGui(Screen):
         self.layout.add_widget(self.label)
 
 class PublisherPanel(BoxLayout):
-    def __init__(self, **kwargs):
+    def __init__(self, coverFileName, **kwargs):
         # make sure we aren't overriding any important functionality
         super(PublisherPanel, self).__init__(**kwargs)
         self.orientation='vertical'
         self.size_hint=(1,1)
-        logo = Image(source='8.jpg')
+        logo = Image(source=coverFileName)
         self.add_widget(logo)
         self.add_widget(Label(text="test label de publisher",size_hint=(1,0.1)))
         self.size_hint_y=None
         self.height=140
+        self.width = 80
 
 class Test(App):
     def build(self):
-        grid = BoxLayout(orientation='horizontal',size_hint=(1, None),height=160)
-        # grid.bind(minimum_height=grid.setter('height'))
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
-        grid.add_widget(PublisherPanel())
+        carousel = Carousel(direction='right')
+
+        grid = GridLayout(cols=4)
+        grid.size_hint_y = None
+        grid.height= 140
+        grid.size_hint_x=None
+        listaImagenes = []
+        for i in range(1,9):
+            print(i)
+            listaImagenes.append(str(i)+".jpg")
+        box=BoxLayout()
+        box.orientation='horizontal'
+        l =  list(range(5,9))
+        print(listaImagenes[0])
+        for i in range(0,4):
+            box.add_widget(PublisherPanel(listaImagenes[i]))
+        carousel.add_widget(box)
+        box = BoxLayout()
+        box.orientation = 'horizontal'
+        for i in range(4, 8):
+            box.add_widget(PublisherPanel(listaImagenes[i]))
+        carousel.add_widget(box)
         scroll = ScrollView(size_hint=(None,1), size=(Window.width, Window.height))
-        scroll.do_scroll_y=False
+        #scroll.do_scroll_y=False
         scroll.add_widget(grid)
-        return scroll
+        return carousel
 
 if __name__ == "__main__":
     test =Test()
